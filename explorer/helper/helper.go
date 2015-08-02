e8dcf18bcaf3bd0c102eff0862eaadb94e410c86
Thomas Rabaix <thomas.rabaix@gmail.com>
2015-08-02 20:45:48 +0200

    add licence headers

--- explorer/helper/helper.go
+++ explorer/helper/helper.go
@@ -1,3 +1,8 @@
+// Copyright © 2014-2015 Thomas Rabaix <thomas.rabaix@gmail.com>.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
 package helper
 
 import (
