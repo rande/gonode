9d380c5cb80e934c44d776d5e0e781eb278473bb
Thomas Rabaix <thomas.rabaix@gmail.com>
2016-07-15 16:20:43 +0200

    feat(ci): add panic test

---
+++ core/helper/panic_test.go
@@ -0,0 +1,48 @@
+// Copyright © 2014-2016 Thomas Rabaix <thomas.rabaix@gmail.com>.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+package helper
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Panic_If_True(t *testing.T) {
+	assert.Panics(t, func() {
+		PanicIf(true, "Panic !!!")
+	})
+}
+
+func Test_Panic_If_False(t *testing.T) {
+	assert.NotPanics(t, func() {
+		PanicIf(false, "Should not panic !!!")
+	})
+}
+
+func Test_Panic_Unless_False(t *testing.T) {
+	assert.Panics(t, func() {
+		PanicUnless(false, "Panic !!!")
+	})
+}
+
+func Test_Panic_Unless_True(t *testing.T) {
+	assert.NotPanics(t, func() {
+		PanicUnless(true, "Should not panic !!!")
+	})
+}
+
+func Test_Panic_Callback(t *testing.T) {
+	called := false
+
+	assert.Panics(t, func() {
+		PanicIf(true, "Panic !!!", func() {
+			called = true
+		})
+	})
+
+	assert.True(t, called)
+}
