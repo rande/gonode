3aa23584b79b8993aaad892ded5bcbd7b5a2d4bf
Thomas Rabaix <thomas.rabaix@gmail.com>
2016-07-15 16:39:45 +0200

    feat(ci): add DefaultViewHandler test

---
+++ modules/debug/core_default_view_test.go
@@ -0,0 +1,36 @@
+// Copyright © 2014-2016 Thomas Rabaix <thomas.rabaix@gmail.com>.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+package debug
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/rande/gonode/modules/base"
+	"github.com/flosch/pongo2"
+)
+
+func Test_Default_View(t *testing.T) {
+	node := &base.Node{
+		Type: "foo.bar",
+	}
+
+	request := &base.ViewRequest{}
+
+	response := &base.ViewResponse{
+		StatusCode:   200,
+		Context:      pongo2.Context{},
+	}
+
+	v := &DefaultViewHandler{}
+
+	err := v.Execute(node, request, response)
+
+	assert.NoError(t, err)
+	assert.Equal(t, node, response.Context["node"])
+	assert.Equal(t, 200, response.StatusCode)
+	assert.Equal(t, "nodes/foo.bar.tpl", response.Template)
+}
