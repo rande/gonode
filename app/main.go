5b16c4646fa7fe1cabafeabaa4023dacc09e5366
Thomas Rabaix <thomas.rabaix@gmail.com>
2017-07-17 23:41:52 +0200

    feat(legal): update date in headers

--- app/main.go
+++ app/main.go
@@ -1,4 +1,4 @@
-// Copyright © 2014-2015 Thomas Rabaix <thomas.rabaix@gmail.com>.
+// Copyright © 2014-2017 Thomas Rabaix <thomas.rabaix@gmail.com>.
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
