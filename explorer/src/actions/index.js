cbfff1eb660819d4045fb6b3cfbab338c18e5171
Thomas Rabaix <thomas.rabaix@gmail.com>
2018-03-12 22:35:04 +0100

    clean(explorer): remove front code

--- explorer/src/actions/index.js
+++
@@ -1,5 +0,0 @@
-export * from './security-actions';
-export * from './nodes-actions';
-export * from './node-actions';
-export * from './node-revisions-actions';
-export default {}; // required for wildcard export… babel bug
--- explorer/src/routing/index.js
+++
@@ -1,5 +0,0 @@
-import routes  from './routes.jsx';
-import history from './history';
-
-export { history as history };
-export { routes as getRoutes };
