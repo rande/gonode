e88bf954074b8cf504065bf3c7bde8034c82a4d0
Thomas Rabaix <thomas.rabaix@gmail.com>
2021-03-16 00:58:19 +0000

    feat(vaul): add more tests

--- core/vault/vault_cipher_test.go
+++ core/vault/vault_cipher_test.go
@@ -16,6 +16,30 @@ var encKey = []byte("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 var invalidKey = []byte("baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 var message = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
 
+func Test_GetCipher(t *testing.T) {
+	encrypter, decrypter := GetCipher("aes_ofb")
+	assert.NotNil(t, encrypter)
+	assert.NotNil(t, decrypter)
+
+	encrypter, decrypter = GetCipher("aes_ctr")
+	assert.NotNil(t, encrypter)
+	assert.NotNil(t, decrypter)
+
+	encrypter, decrypter = GetCipher("aes_cbc")
+	assert.NotNil(t, encrypter)
+	assert.NotNil(t, decrypter)
+
+	encrypter, decrypter = GetCipher("aes_gcm")
+	assert.NotNil(t, encrypter)
+	assert.NotNil(t, decrypter)
+
+	encrypter, decrypter = GetCipher("no_op")
+	assert.NotNil(t, encrypter)
+	assert.NotNil(t, decrypter)
+
+	assert.Panics(t, func() { GetCipher("invalid_cypher") })
+}
+
 func Test_AesOFB(t *testing.T) {
 	ve := NewVaultElement()
 	ve.MetaKey = []byte("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
---
+++ core/vault/vault_driver_fs_test.go
@@ -0,0 +1,55 @@
+// Copyright © 2014-2021 Thomas Rabaix <thomas.rabaix@gmail.com>.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+package vault
+
+import (
+	"io/ioutil"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"fmt"
+	"os"
+)
+
+// this is just a test to validata how the aws sdk behave
+func Test_Vault_Driver_Fs(t *testing.T) {
+	// init vault
+	v := &DriverFs{
+		Root: fmt.Sprintf("%s/gonode/%d", os.TempDir(), time.Now().Nanosecond()),
+	}
+
+	key := "test/assd"
+
+	assert.False(t, v.Has(key))
+
+	data := []byte("foobar et foo")
+
+	w, err := v.GetWriter(key)
+
+	assert.Nil(t, err)
+
+	w.Write(data)
+	w.Close()
+
+	// should have the file
+	assert.True(t, v.Has(key))
+
+	r, err := v.GetReader(key)
+	assert.Nil(t, err)
+
+	data, err = ioutil.ReadAll(r)
+
+	assert.Nil(t, err)
+	assert.Equal(t, []byte("foobar et foo"), data)
+
+	err = v.Remove(key)
+	assert.Nil(t, err)
+
+	err = v.Remove(key)
+	assert.NotNil(t, err)
+}
